e7fc70516ee0c5294bad352bc88cd8e08a68df7b
takinaaakawaii <123395074+takinaaakawaii@users.noreply.github.com>
2023-04-09 17:59:40 +0700

    Update ditmeanhquoc.cpp

--- ditmeanhquoc.cpp
+++ ditmeanhquoc.cpp
@@ -1,6 +1,18 @@
+// Đây là câu lệnh để chửi anh Quốc
+
 #include <iostream>
 using namespace std;
 int main() {
 	cout << "Dit me anh Quoc";
+	return 0;
+}
+
+// Đây là câu lệnh thứ 2 để chửi chết mẹ anh Quốc
+
+#include <iostream>
+using namespace std;
+int main() {
+    string name = "Quoc";
+	cout << "Dit me anh " << name;
 	return 0;
 }
